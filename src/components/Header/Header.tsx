2434483248e4761ab64c4c0a42dbb65c7ea4b86e
jyh2610 <jogg4177@naver.com>
2023-12-11 16:02:08 +0900

    feat: nav dropDown

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
+import { Dropdown, type MenuProps } from "antd";
 import Link from "next/link";
+
 import "./style.css";
 
 const menuItems = [
@@ -6,6 +8,33 @@ const menuItems = [
   { text: "Explore", href: "#", submenu: true },
   { text: "Business", href: "#", submenu: true },
   { text: "Pricing", href: "#" },
+];
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        2nd menu item
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+        3rd menu item
+      </a>
+    ),
+  },
 ];
 
 function Header() {
@@ -20,15 +49,25 @@ function Header() {
           </div>
           <div className="gnb">
             <ul className="flex gap-[24px]">
-              {menuItems.map((item) => (
-                <li key={item.text}>
-                  <Link className="text-[1rem]" href={item.href}>
-                    {item.text}
-                    {item.submenu && <i className="arrow">1</i>}
-                  </Link>
-                  {item.submenu && <ul className="deep-menu"></ul>}
-                </li>
-              ))}
+              {menuItems.map((item) =>
+                item.submenu === true ? (
+                  <Dropdown key={item.text} menu={{ items }}>
+                    <li>
+                      <Link className="text-[1rem]" href={item.href}>
+                        {item.text}
+                        {item.submenu && <i className="arrow">1</i>}
+                      </Link>
+                    </li>
+                  </Dropdown>
+                ) : (
+                  <li key={item.text}>
+                    <Link className="text-[1rem]" href={item.href}>
+                      {item.text}
+                      {item.submenu && <i className="arrow">1</i>}
+                    </Link>
+                  </li>
+                ),
+              )}
             </ul>
           </div>
           <div className="user-wrap">
