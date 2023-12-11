bdb6008ee51d26bf68d31f1aabbb0f4e49862a0e
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-11 16:06:00 +0900

    Fix: 이미지 추가

--- src/components/Pageconfigurator/SectionTwo.tsx
+++ src/components/Pageconfigurator/SectionTwo.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import { configuratordata } from "@/constants/3dconfiguratordata";
 
+import SectionImage from "../Section/SectionImage";
 import SectionTitleAndContent from "../Section/SectionTitleAndContent";
 
 const SectionTwo = () => {
   return (
     <div>
       {configuratordata.map((data) => (
-        <SectionTitleAndContent key={data.id} contenttitle={data.textdata[1].title} content={data.textdata[1].content} />
+        <>
+          <SectionTitleAndContent key={data.id} contenttitle={data.textdata[1].title} content={data.textdata[1].content} />
+          <SectionImage sectionimg={""} />
+        </>
       ))}
     </div>
   );
