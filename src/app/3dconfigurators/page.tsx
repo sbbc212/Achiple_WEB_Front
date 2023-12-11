36f220ac4bb571ee8bd17ac2efe04c65dbc7b51f
jyh2610 <jogg4177@naver.com>
2023-12-11 17:19:14 +0900

    파일명 변경

--- src/app/3dconfigurators/page.tsx
+++ src/app/3dconfigurators/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-import SectionTwo from "@/components/Pageconfigurator/sectiontwo";
+import SectionTwo from "@/components/Pageconfigurator/SectionTwo";
+import PageTopSection from "@/components/Section/PageTopSection";
 import TextImageCard from "@/components/textimagecard/TextImageCard";
-
-import Page_top_section from "@/components/Section/Page_top_section";
 
 const page = () => {
   return (
     <div className="layout">
-      <Page_top_section />
+      <PageTopSection />
       <SectionTwo />
       <TextImageCard type="imgright" />
       <TextImageCard type="imgleft" />
