d96875f5c8aec9263a7ee522058d85fbfe9cee8f
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-11 15:58:13 +0900

    Fix: 컴포넌트 분리 및 적용

--- src/app/3dconfigurators/page.tsx
+++ src/app/3dconfigurators/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import SectionTwo from "@/components/Pageconfigurator/sectiontwo";
 import Page_top_section from "@/components/Section/Page_top_section";
 import TextImageCard from "@/components/textimagecard/TextImageCard";
 
@@ -7,6 +8,7 @@ const page = () => {
   return (
     <div className="layout">
       <Page_top_section />
+      <SectionTwo />
       <TextImageCard type="imgright" />
       <TextImageCard type="imgleft" />
     </div>
