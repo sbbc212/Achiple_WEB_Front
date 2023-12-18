9ebdc3f6105eea5285961816487601035a778b59
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-18 14:43:36 +0900

    Fix: 임포트 및 타입 수정

--- src/components/Animation/AnimationSecFour.tsx
+++ src/components/Animation/AnimationSecFour.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-import { animationtextdata } from "@/constants/animation";
+import { secfouranimation } from "@/constants/animation";
 
 import TextCard from "../Cardlayout/TextCard";
 
@@ -8,7 +6,7 @@ const AnimationSecFour = () => {
   return (
     <>
       <div className="flex justify-between pb-9 pt-9">
-        {animationtextdata.map((item) => (
+        {secfouranimation.map((item) => (
           <TextCard key={item.id} text={item} btnType={false} btnText={""} />
         ))}
       </div>
