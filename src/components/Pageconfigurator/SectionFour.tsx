d873602e48438e9f529b2a9a89a4f95bcf5f0b51
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-19 15:24:23 +0900

    Fix: 이미지 텍스트 카드에 버튼 추가

--- src/components/Pageconfigurator/SectionFour.tsx
+++ src/components/Pageconfigurator/SectionFour.tsx
@@ -11,7 +11,7 @@ const SectionFour = () => {
     <>
       <div className="section-tit text-center mb-[60px]" style={{ padding: "30px 60px 0 60px" }}>
         <SectionTitleAndContent content={configuratordata[0].textdata[2]} />
-      </div>{" "}
+      </div>
       <TextImageCard data={carddata.slice(0, 1)} />
     </>
   );
