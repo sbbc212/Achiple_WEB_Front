cb2011619103c9f706100393c6d609c678af6625
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-19 11:30:03 +0900

    Fix: 컨피규 카드 로직수정

--- src/components/Pageconfigurator/SectionThree.tsx
+++ src/components/Pageconfigurator/SectionThree.tsx
@@ -1,19 +1,27 @@
-import { configuratordata } from "@/constants/3dconfiguratordata";
+import { configuratordata, nobtncardData } from "@/constants/3dconfiguratordata";
 
-import SectionCard from "./SectionCard";
+import ThreeCardGroup from "./ThreeCardGroup";
 import Linebtn from "../Button/Linebtn";
 import SectionTitleAndContent from "../Section/SectionTitleAndContent";
 
+import type { maincardDataType } from "@/constants/maindata";
+
 const SectionThree = () => {
-  const sectionCards = [];
-  for (let i = 0; i < 3; i++) {
-    sectionCards.push(<SectionCard key={i} content={configuratordata} />);
+  const dataSize = 3;
+  const ThreeLengthData: maincardDataType[][] = [];
+
+  for (let i = 0; i < nobtncardData.length; i += dataSize) {
+    ThreeLengthData.push(nobtncardData.slice(i, i + dataSize));
   }
   return (
     <>
       <div className="section-tit text-center mb-[60px]" style={{ padding: "30px 60px 0 60px" }}>
         <SectionTitleAndContent content={configuratordata[0].textdata[4]} />
-        {sectionCards}
+        {ThreeLengthData.map((item, i) => (
+          <ul key={item[i].id} className="flex text-start justify-between pb-[60px]">
+            <ThreeCardGroup content={item} />
+          </ul>
+        ))}
         <Linebtn text={"Archiple 도움말 섹션에서 자세히 알아보세요."} />
       </div>
     </>
--- src/components/Pageconfigurator/ThreeCardGroup.tsx
+++ src/components/Pageconfigurator/ThreeCardGroup.tsx
@@ -5,9 +5,13 @@ import type { maincardDataType } from "@/constants/maindata";
 const ThreeCardGroup = ({ content }: { content: maincardDataType[] }) => {
   return (
     <>
-      {content.map((cardlist, index) => (
-        <Card key={index} cardlist={cardlist} btntext={"btnText"} />
-      ))}
+      {content.map((cardlist, index) =>
+        cardlist.btnText === "" ? (
+          <Card isBtn={true} key={index} cardlist={cardlist} btntext={"btnText"} />
+        ) : (
+          <Card key={index} cardlist={cardlist} btntext={"btnText"} />
+        ),
+      )}
     </>
   );
 };
