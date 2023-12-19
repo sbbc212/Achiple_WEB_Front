c84d263ef1a3ef13f1f3d41a854af9db11f93365
jyh2610 <jogg4177@naver.com>
2023-12-19 16:57:07 +0900

    fix: 폴더명 변경

--- src/app/Pricing/page.tsx
+++ src/app/Pricing/page.tsx
@@ -1,14 +1,36 @@
-"'use client ";
+"use client";
 
 import React from "react";
 
 import HomePage from "@/components/Accordion/HomePage";
 import Tab_menu from "@/components/Tabmenu/Tab_menu";
 
-function page() {
+export interface tabData {
+  id: number;
+  title: string;
+  pricing: string;
+  description: string;
+}
+
+function Pricingpage() {
+  const data: tabData[] = [
+    {
+      id: 0,
+      title: "월간",
+      pricing: "$19",
+      description: "30일 평가판 기간 이후에는 매월 청구 됩니다.",
+    },
+    {
+      id: 1,
+      title: "연간",
+      pricing: "$15",
+      description: "30일 평가판 기간 이후에는 매월 청구 됨, 연간 $180",
+    },
+  ];
+
   return (
     <div className="layout">
-      <div className="contant" style={{ height: "100vh" }}>
+      <div className="contant">
         <div className="section-tit text-center" style={{ padding: "120px 60px 0 60px" }}>
           <h3 className="text-[68px] mb-[14px]" style={{ fontWeight: "600" }}>
             가격
@@ -18,11 +40,11 @@ function page() {
             <br /> 지금 30일 무료 평가판을 시작해 보세요 .
           </p>
         </div>
-        <Tab_menu />
+        <Tab_menu data={data} />
         <HomePage />
       </div>
     </div>
   );
 }
 
-export default page;
+export default Pricingpage;
