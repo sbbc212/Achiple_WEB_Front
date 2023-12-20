a98faade9572e3b284bda4af66ee7aa43f33ddf0
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-20 17:15:12 +0900

    Fix: 이미지 사이즈 제거

--- src/components/textimagecard/ImageCard.tsx
+++ src/components/textimagecard/ImageCard.tsx
@@ -1,7 +1,7 @@
 const ImageCard = ({ dataimg }: { dataimg: string }) => {
   return (
-    <div className="w-1/2 h-full">
-      <img src={dataimg} className="w-[50rem] h-[25rem]" alt="카드 이미지 임시" />
+    <div className="w-1/2">
+      <img src={dataimg} alt="카드 이미지 임시" />
     </div>
   );
 };
