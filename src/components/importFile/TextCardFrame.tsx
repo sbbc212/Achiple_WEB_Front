bf325ae6e2192dfbbf65443d23fa482a40365e11
jyh2610 <jogg4177@naver.com>
2023-12-20 13:34:40 +0900

    fix: 레이아웃 수정

--- src/components/importFile/TextCardFrame.tsx
+++ src/components/importFile/TextCardFrame.tsx
@@ -6,7 +6,7 @@ export default function TextCardFrame() {
   return (
     <div>
       <h2>벡타리 성공 사례에서 영감 얻기</h2>
-      <div className="layout space-y-4">
+      <div className="layout">
         <TextImageCard data={carddata} />
         <TextImageCard data={carddata} />
         <TextImageCard data={carddata} />
