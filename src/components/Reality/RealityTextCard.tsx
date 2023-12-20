acfaa265930060753ba26cce4bfb8e7fae302eda
kim-teak-yong <144188911+tackyong@users.noreply.github.com>
2023-12-20 14:50:18 +0900

    병합완료

--- src/components/Reality/RealityTextCard.tsx
+++ src/components/Reality/RealityTextCard.tsx
@@ -4,7 +4,7 @@ import type { textCardType } from "@/constants/maindata";
 
 const RealityTextCard = ({ realitytextdata }: { realitytextdata: textCardType[] }) => {
   return (
-    <div className="layout flex justify-between gap-5 pb-9 pt-9">
+    <div className=" flex justify-between gap-5 pb-9 pt-9">
       {realitytextdata.map((item) => (
         <TextCard key={item.id} text={item} btnType={false} btnText={""} />
       ))}
