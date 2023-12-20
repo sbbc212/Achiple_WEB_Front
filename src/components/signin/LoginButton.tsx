94b0de06ffab41eca641453ecaa74bab27fc9c66
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-20 17:07:01 +0900

    Fix: 취소버튼 수정

--- src/components/signin/LoginButton.tsx
+++ src/components/signin/LoginButton.tsx
@@ -11,22 +11,23 @@ const LoginButton = () => {
   };
 
   return (
-    <>
+    <div className="py-[1.2rem]">
       <Form.Item>
-        <Button style={{ padding: "1.6rem", lineHeight: 0 }} type="primary" htmlType="submit" className="btn btn-primary login-form-button mb-[0.5rem]" block>
+        <Button style={{ padding: "1.6rem", lineHeight: 0 }} type="primary" htmlType="submit" className="btn btn-primary login-form-button" block>
           로그인
         </Button>
+      </Form.Item>
+      <Form.Item>
         <Button
           style={{ padding: "1.6rem", lineHeight: 0, border: "1px solid #d9d9d9", color: "#434343" }}
           type="link"
-          htmlType="submit"
           onClick={clickCancel}
           className="btn btn-primary login-form-button"
           block>
           취소
         </Button>
       </Form.Item>
-    </>
+    </div>
   );
 };
 export default LoginButton;
