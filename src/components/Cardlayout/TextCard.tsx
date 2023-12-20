df6a5cf36e5b6aac1d6214cf147e2e2e4198a23c
DevHyunJ <sonhyunji9819@gmail.com>
2023-12-20 16:02:13 +0900

    Fix: 텍스트 사이즈 ㅈ수정

--- src/components/Cardlayout/TextCard.tsx
+++ src/components/Cardlayout/TextCard.tsx
@@ -7,7 +7,7 @@ function TextCard({ text, btnType, btnText }: { text: textCardType; btnType: boo
   return (
     <li className={styles.text_layout}>
       <span className={`block font-bold ${text?.textgray === "true" ? "text-[24px]" : "text-[18px]"}  mb-[14px]`}>{text?.title}</span>
-      <p className={`${text?.textgray === "true" ? "text-[1.2rem] text-gray-500" : ""} my-[24px]`}>{text?.content}</p>
+      <p className={`${text?.textgray === "true" ? "text-[1.1rem] text-gray-500" : ""} my-[24px]`}>{text?.content}</p>
       {btnType === true && <Linebtn text={btnText} />}
     </li>
   );
